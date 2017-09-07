70a8569bf1926380237bc5de36df3a7f3e21f1c1
lukehayes <lukepurplehayes@gmail.com>
2017-09-07 19:02:36 +0100

    Added basic working code

---
+++ resizer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from PIL import Image
+import glob, os, sys, getopt
+
+currentDir = os.getcwd()
+width = 1200
+finalDir = "Done/"
+
+for i in glob.glob("*.jpg"):
+	img = Image.open(i)
+	height = width * img.size[1] / img.size[0]
+	name = os.path.splitext(i)[0]
+	fileType = os.path.splitext(i)[1]
+	rsz = img.resize((width,height))
+	rsz.save(finalDir + name + fileType)
